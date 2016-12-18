b93d54c7e4258211a3622326c77f7f7e7daac16c
Sarthak Srinivas <sarthaksrinivas@gmail.com>
2016-12-18 16:47:39 +0530

    Added Pomodoro class

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ while True:
     t = time()
     print(t)
     sleep(1)
+
+class Pomodoro(object):
+    """The Main Pomodoro Class Timer."""
+    # Instance Variables
+    pass
+
+    # Functions
+    def __init__(self):
+        pass
+    def __del__(self):
+        pass
+
+
